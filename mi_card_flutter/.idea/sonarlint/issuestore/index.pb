1cda3e324ea574c328ab2525d82f966cf78a0623
Ruyonga Daniel <druyonga@gmail.com>
2022-09-13 23:20:06 +0300

    add ami card

---
+++ mi_card_flutter/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+<
+pubspec.lock,c/1/c1c78778a0cef1e5344166792d1d823fdcb8b109
---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/nurse.jpg'),
+              ),
+              Text(
+                'Teheca Nurse',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'LEAD PATIENT CARE ASSISTANT',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 30,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+256 200 902468',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro', fontSize: 20.0),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 30,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'test@teheca.com',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro', fontSize: 20.0),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
